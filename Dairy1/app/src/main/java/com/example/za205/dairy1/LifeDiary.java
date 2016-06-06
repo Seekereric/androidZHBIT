cf8a5a57bae58375dc01889f0d09fc88613bff2c
Seekereric <seekereric@hotmail.com>
2016-06-06 20:28:55 +0800

    Dairy1:add all xml

---
+++ Dairy1/app/src/main/java/com/example/za205/dairy1/LifeDiary.java
@@ -0,0 +1,15 @@
+package com.example.za205.dairy1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LifeDiary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.diary_list);
+
+
+    }
+}
